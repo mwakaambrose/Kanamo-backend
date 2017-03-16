de5c8942e4b240e8bee7db0e88efcc4dd6d3de9c
Mwaka Ambrose <ambrosemwaka@gmail.com>
2017-03-16 18:07:05 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Kanamo Backend 1.5
                 </div>
 
                 <div class="links">
