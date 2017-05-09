66f47e2cde7834ad08a8c3cf16863ec43584cd8d
Mwaka Ambrose <ambrosemwaka@gmail.com>
2017-05-09 20:13:56 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,91 +1,103 @@
 <!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"><![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"><![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"><![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"><!--<![endif]-->
+<head>
+<meta charset="UTF-8">
+<title>Kanamo - Cloud Under developement</title>
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<link rel="shortcut icon" href="images/ico/favicon.png">
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<!--[if IE]><![endif]-->
+<link rel="stylesheet" href="/css/style.css">
+<script src="/js/jquery.js"></script>
+<script src="/js/countdown.js"></script>
+<script src="/js/uikit.scrollspy.js"></script>
+<script src="j/s/scripts.js"></script>
+<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body id="backtotop">
 
-        <title>Laravel</title>
+<div class="fullwidth clearfix">
+	<div id="topcontainer" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
+		
+		<p><span class="fa fa-signal"></span></p>
+		<h1><span>Kanamo</span><br />is coming soon</h1>
+		<p>It's almost ready ... honest</p>
+		
+	</div>
+</div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<div class="arrow-separator arrow-white"></div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+<div class="fullwidth colour1 clearfix">
+	<div id="countdown" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
 
-            .full-height {
-                height: 100vh;
-            }
+		<div id="countdowncont" class="clearfix">
+			<ul id="countscript">
+				<li>
+					<span class="days">00</span>
+					<p>Days</p>
+				</li>
+				<li>
+					<span class="hours">00</span>
+					<p>Hours</p>
+				</li>
+				<li class="clearbox">
+					<span class="minutes">00</span>
+					<p>Minutes</p>
+				</li>
+				<li>
+					<span class="seconds">00</span>
+					<p>Seconds</p>
+				</li>
+			</ul>
+		</div>
+	
+	</div>
+</div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<div class="arrow-separator arrow-theme"></div>
 
-            .position-ref {
-                position: relative;
-            }
+<div class="fullwidth colour2 clearfix">
+	<div id="maincont" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+		<p><strong>Lorem ipsum dolor</strong> sit amet, consectetur adipiscing elit. <a title="" href="#">Donec quis enim nulla</a>. Suspendisse posuere arcu ac eros iaculis egestas commodo risus tempus. Fusce placerat nulla nisi. Proin congue erat non orci adipiscing nec porttitor lacus egestas. <em>Donec venenatis</em>, risus et hendrerit ultrices, libero sem lacinia risus, ut convallis massa sapien nec libero. Suspendisse arcu orci, gravida nec aliquet ut, lacinia non nisl.</p>
+        <h2>Sign up and we'll let you know when we launch</h2>
+        <p>If you would like to receive <strong>news and special offers</strong> please send us your email address below:</p>
+		<div id="signupform" class="sb-search clearfix">
+			<form method="post" id="contact" class="clearfix">
+				<input class="sb-search-input" placeholder="Enter email ..." type="text" value="">
+				<input class="sb-search-submit" value="" type="submit">
+				<button class="formbutton" type="submit"><span class="fa fa-arrow-right"></span></button>
+			</form>
+		</div>
+	
+	</div>
+</div>
 
-            .content {
-                text-align: center;
-            }
+<div class="arrow-separator arrow-themelight"></div>
 
-            .title {
-                font-size: 84px;
-            }
+<div class="fullwidth clearfix">
+	<div id="footercont" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Kanamo Backend 1.5
-                </div>
-
-                <div class="links">
-                    <a href="http://kanamo.biz/">Cloud under developement</a>
-                </div>
-            </div>
-        </div>
-    </body>
+		<p class="backtotop"><a title="" href="#backtotop"><span class="fa fa-angle-double-up"></span></a></p>
+		<div id="socialmedia" class="clearfix">
+			<ul>
+				<li><a title="" href="#" rel="external"><span class="fa fa-facebook"></span></a></li>
+				<li><a title="" href="#" rel="external"><span class="fa fa-twitter"></span></a></li>
+				<li><a title="" href="#" rel="external"><span class="fa fa-google-plus"></span></a></li>
+				<li><a title="" href="#" rel="external"><span class="fa fa-linkedin"></span></a></li>
+				<li><a title="" href="#" rel="external"><span class="fa fa-pinterest"></span></a></li>
+			</ul>
+		</div>
+		<p>Kanamo is powered by <a title="download website templates" href="http://www.facebook.com/TrustFinity" rel="external">TrustFinity</a> - &copy 2017 </p>
+	</div>
+</div>
+</body>
 </html>
