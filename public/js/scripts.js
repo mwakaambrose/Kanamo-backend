d929f89b2a8da22e4f61176b8682ac9ed170d6f9
Mwaka Ambrose <ambrosemwaka@gmail.com>
2017-05-09 20:19:03 +0300

    Updates js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,7 @@
 /** External Links **/
 
+console.log('You have reached');
+
 function externalLinks() { if (!document.getElementsByTagName) return; var anchors = document.getElementsByTagName("a"); for (var i=0; i<anchors.length; i++) { var anchor = anchors[i]; if (anchor.getAttribute("href") && anchor.getAttribute("rel") == "external") anchor.target = "_blank"; } } window.onload = externalLinks;
 
 /** jQuery Easing **/
