fbdddaf331f1c82deac12b75c75f77b2e3c9761d
vagrant <vagrant@homestead>
2017-05-13 09:46:39 +0000

    update api route

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'facebook'], function () {
+Route::group(['prefix' => 'v1/facebook'], function () {
     Route::get('message', 'FacebookController@facebookVerify');
     Route::post('message', 'FacebookController@message');
 });
