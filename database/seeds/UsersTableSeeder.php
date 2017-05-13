53f1205c862503bae4a6c6884a1a9577557f1224
Jamal Alawes <jamal.alawes@gmail.com>
2017-05-13 10:15:22 +0300

    Adds Post Model and Table

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        User::create([
+            'name' => 'Jamal Alawes',
+            'email' => 'jamal@jamal.com',
+            'password' => bcrypt('secret'),
+            'business_name' => 'Kanamo',
+            'address' => '833 Homer St. Vancouver, BC',
+        ]);
+
+        User::create([
+            'name' => 'Mwaka Ambrose',
+            'email' => 'mwkams@ambrose.com',
+            'password' => bcrypt('secret'),
+            'business_name' => 'Kanamo',
+            'address' => 'Kamapala, UG',
+        ]);
+
+    }
+}
