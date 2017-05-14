1ff60c56e197e7b839be305308ae69bbc1a3edf4
Corey Sanford <corey@ensibuuko.com>
2017-05-14 07:26:32 +0000

    Finishes landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,16 +7,69 @@
 <div class="fullwidth clearfix">
 	<div id="topcontainer" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
 
-		<p><span class="fa fa-signal"></span></p>
-		<h1><span>Kanamo</span><br />is coming soon</h1>
-		<p>It's almost ready ... honest</p>
+		<div class="row">
+			<div class="col-xs-12 col-sm-4">
+				<div class="spacer hidden-xs"><br><br><br></div>
+
+				<p><span class="fa fa-signal"></span></p>
+				<h1><span>Kanamo</span><br /></h1>
+				<h3 class="alt"><em>Find your Happy Hour</em></h3>				
+				<div class="spacer visible-xs"><br><br></div>
+			</div>
+			<div class="col-xs-12 col-sm-8">
+				<img class="img-responsive" src="/imgs/laptop-mobile-graphic-trimmed.png" />
+			</div>
+		</div>
 
 	</div>
 </div>
 
+<h2>This week's most popular</h2>
+<div class="row popular-deals">
+	@foreach([
+		[
+			'img_src' => 'kanamo-que-pasa-600-200.jpg', 
+			'bar_name' => 'Que Pasa?', 
+			'url' => 'https://www.facebook.com/quepasakampala/',
+			'deal' => '2-for-1 Toxicos, UGX 2000 Tuskers',
+			'date' => 'Every Wednesday, 4-7pm'
+		],[
+			'img_src' => 'kanamo-governor-600-200.jpg',
+			'bar_name' => 'The Governor', 
+			'url' => 'https://www.instagram.com/explore/locations/288852266/',
+			'deal' => 'One drink free with entry before 11pm',
+			'date' => 'Monday to Thursday'
+		],[
+			'img_src' => 'kanamo-cedars-600-200.jpg',
+			'bar_name' => 'Cedars', 
+			'url' => 'https://www.tripadvisor.com/Restaurant_Review-g293841-d12138333-Reviews-Cedars-Kampala_Central_Region.html',
+			'deal' => 'Bottles of wine at UGX 50,000',
+			'date' => 'Everyday'
+		]
+	] as $section)
+		<div class="col-xs-12 col-sm-4">
+			<a 		class="popular"
+					href="{{ $section['url'] }}">
+				<div class="img-pane">
+					<img 	src="/imgs/{{ $section['img_src'] }}" 
+							class="img-responsive" />
+				</div>
+				<div class="content">
+					<h3 class="bar">{{ $section['bar_name'] }}</h3>
+					<p class="deal">{!! $section['deal'] !!}</p>
+					<p class="date">{{ $section['date'] }}</p>
+				</div>
+			</a>
+		</div>
+	@endforeach
+</div>
+<br>
+<br>
+<br>
+
 <div class="arrow-separator arrow-white"></div>
 
-<div class="fullwidth colour1 clearfix">
+{{-- <div class="fullwidth colour1 clearfix">
 	<div id="countdown" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
 
 		<div id="countdowncont" class="clearfix">
@@ -41,27 +94,27 @@
 		</div>
 
 	</div>
-</div>
+</div> --}}
 
 <div class="arrow-separator arrow-theme"></div>
 
-<div class="fullwidth colour2 clearfix">
-	<div id="maincont" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
-
-		<p><strong>Lorem ipsum dolor</strong> sit amet, consectetur adipiscing elit. <a title="" href="#">Donec quis enim nulla</a>. Suspendisse posuere arcu ac eros iaculis egestas commodo risus tempus. Fusce placerat nulla nisi. Proin congue erat non orci adipiscing nec porttitor lacus egestas. <em>Donec venenatis</em>, risus et hendrerit ultrices, libero sem lacinia risus, ut convallis massa sapien nec libero. Suspendisse arcu orci, gravida nec aliquet ut, lacinia non nisl.</p>
-        <h2>Sign up and we'll let you know when we launch</h2>
-        <p>If you would like to receive <strong>news and special offers</strong> please send us your email address below:</p>
-		<div id="signupform" class="sb-search clearfix">
-			<form method="post" id="contact" class="clearfix">
-				<input class="sb-search-input" placeholder="Enter email ..." type="text" value="">
-				<input class="sb-search-submit" value="" type="submit">
-				<button class="formbutton" type="submit"><span class="fa fa-arrow-right"></span></button>
-			</form>
+<div class="row fullwidth signup colour2 clearfix">
+	<div class="col-xs-12">
+		<div class="">
+			<div id="maincont" class="bodycontainer clearfix" data-uk-scrollspy="{cls:'uk-animation-fade', delay: 300, repeat: true}">
+		        <h2>Want drinks at great prices?</h2>
+		        <p>We'll let you know!</p>
+				<div id="signupform" class="sb-search clearfix">
+					<form method="post" id="contact" class="clearfix">
+						<input class="sb-search-input" placeholder="Enter email ..." type="text" value="">
+						<input class="sb-search-submit" value="" type="submit">
+						<button class="formbutton" type="submit"><span class="fa fa-arrow-right"></span></button>
+					</form>
+				</div>
+			</div>
 		</div>
-
 	</div>
 </div>
-
 <div class="arrow-separator arrow-themelight"></div>
 
 <div class="fullwidth clearfix">
