734068238c22c1b8927ecffb86807a33f27d4e6c
Mwaka <you@example.com>
2017-05-14 08:38:43 +0000

    create events

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
 
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
 
             $table->string('address')->nullable();
+            $table->string('business_name')->nullable();
 
             $table->rememberToken();
             $table->timestamps();
--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -11,18 +11,27 @@
         </div>
 
         <div class="panel-body">
-           <form>
+           <form method="post" action="/events">
+                {{ csrf_field() }}
                 <div class="form-group">
                     <label for="title">Event Title</label>
-                    <input type="text" class="form-control" id="title" aria-describedby="titlehelp" placeholder="Enter the title of your event">
+                    <input name="title" type="text" class="form-control" id="title" aria-describedby="titlehelp" placeholder="Enter the title of your event">
+                </div>
+
+                <div class="form-group">
+                    <label for="start_time">Event start time</label>
+                    <input name="start_time" type="text" class="form-control" id="start_time" placeholder="Enter the start time of your event">
+                    
+                    <label class="class="col-md-6" for="stop_time">Event stop time</label>
+                    <input name="end_time" type="text" class="form-control class="col-md-6"" id="stop_time" placeholder="Enter the stop_time of your event">
                 </div>
 
                 <div class="form-group">
                     <label for="description">Event Description</label>
-                    <textarea class="form-control" id="description" rows="3" placeholder="Please provide some information about your event. Who is your target audience? Are you serving food?"></textarea>
+                    <textarea name="description" class="form-control" id="description" rows="3" placeholder="Please provide some information about your event. Who is your target audience? Are you serving food?"></textarea>
                 </div>
 
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-success">Post Event</button>
 
             </form>
         </div>
